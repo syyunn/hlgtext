2979940921f2358b3ffcc1e6d16c1e1b16aa97e1
Zachary <syyun@snu.ac.kr>
2019-06-14 09:07:06 +0900

    Get rid of Server Side

--- popup.js
+++ popup.js
@@ -1,8 +1,18 @@
 chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
-    document.write(selection[0]); //document.write writes html expressions to a document
-    var text = '<form action="https://localhost:8080/example" method="post" id="hlgt_form"><input type="hidden" id = "hlgt" value= ""> </form>';
-    document.write(text)
+
+    document.write(selection[0]);
+
+    var text =
+        '<form action="http://localhost:8080/example" method="post" id="hlgt_form">' +
+        '<input type="hidden" id = "hlgt" name = "hlgt" value= ""> ' +
+        '</form>';
+
+    document.write(text);
+
     document.getElementById('hlgt').value = selection[0];
+    // it stores highlights into value of <input>
+
+    document.getElementById('hlgt_form').submit();
 });
--- submit.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('hlgt_form').submit();
